f32c0a98e43b6b4ce619af1bc71edd1e3bef52bb
d-ecember <119058761+d-ecember@users.noreply.github.com>
2024-10-11 11:40:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ function generatePoem(event) {
     let context = "You are a romatic extpert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructionsDo not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-        axios.get(apiUrl).then(dispalyPoem); 
+        axios.get(apiURL).then(dispalyPoem); 
 
    
 //
